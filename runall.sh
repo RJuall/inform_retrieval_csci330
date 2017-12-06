88f55353a5b7f5224ea9f4df01c8f9932cef54df
Robert Juall <RJuall@gmail.com>
2017-12-06 11:50:58 -0500

    Adapted runall.sh

--- runall.sh
+++ runall.sh
@@ -2,18 +2,19 @@
 
 # bash runall.sh
 
-qdir="queries"
+# qdir="queries"
 qdir="practice_queries" # comment out to run test
 queries=`ls $qdir`
-corpus="book_test"
+# corpus="book_test"
 corpus="book_practice" # comment out to run test
+
+python little_search_rj.py -df $corpus
 
 for f in $queries; do
 	q=`basename $f .txt`
 	q=`echo $q | tr "-" " "`
 	echo " ================== " $q " ================== " 
 	# TODO: call your program here
-	perl tfidf.pl $corpus "$q" $qdir/$f
+	python little_search_rj.py  -q "$q" -r $qdir/$f
 	echo; echo
 done
-
