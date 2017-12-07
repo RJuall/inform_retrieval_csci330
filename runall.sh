a9fbf251177a35e1e84d9e5e09c8d77946ba74e6
Robert Juall <RJuall@gmail.com>
2017-12-07 03:56:37 -0500

    Added files for quiz, adjusted runall script

--- runall.sh
+++ runall.sh
@@ -2,11 +2,11 @@
 
 # bash runall.sh
 
-# qdir="queries"
-qdir="practice_queries" # comment out to run test
+qdir="queries"
+# qdir="practice_queries" # comment out to run test
 queries=`ls $qdir`
-# corpus="book_test"
-corpus="book_practice" # comment out to run test
+corpus="book_test"
+# corpus="book_practice" # comment out to run test
 
 python little_search_rj.py -df $corpus
 
